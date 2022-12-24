b56e0aa8102577787230ccea2a0954b0fa4e05d0
DylanLai67 <dylanl67@bu.edu>
2022-12-23 19:39:55 -0500

    Setup basic routing

--- DLRY/views.py
+++ DLRY/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponse, Http404
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+from django.utils import timezone
 # Create your views here.
+
+
+def home_page(request):
+    context = dict()
+    return render(request, 'DLRY/home_page.html', context)
