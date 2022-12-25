6071ab5984c8d9c60df7028e2e10b5e54f1c330f
Rong Feng Ye <rongfeny@andrew.cmu.edu>
2022-12-25 17:50:57 -0500

    videos are uploaded to db

--- DLRY/models.py
+++ DLRY/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class SideQuest(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.PROTECT) # Either DL, RY
+    # creator = models.ForeignKey(User, on_delete=models.PROTECT) # Either DL, RY
     title = models.CharField(blank=True, max_length=200)
     description = models.CharField(blank=True, max_length=200)
     video = models.FileField(upload_to='uploads/')
